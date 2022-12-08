6ec342c00d3bdb0f2a5c325f82890de04600ad0b
Gabriel laptop <gfreundt@gmail.com>
2022-12-08 11:41:51 -0500

    preview

--- controller/controller.py
+++ controller/controller.py
@@ -17,12 +17,11 @@ from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 
 def load_storyboard(flat):
     """Load JSON file that holds all media information"""
-
-    color1 = [1, 0.1, 0.2, 1]
 
     # get absolute path depending on OS
     directories = {
@@ -39,40 +38,33 @@ def load_storyboard(flat):
     active = [i for i in storyboard if i["active"]]
     inactive = [i for i in storyboard if not i["active"]]
 
-    # flatten information into plain list
-    # ("alert", color1, r["playback"]["file_name"]),
-    if flat:
-        size = "[size=14]"
-        active = [
-            (
-                f'{size}{str(r["position"])}',
-                f'{size}{r["playback"]["aka"]}',
-                f'{size}{r["playback"]["file_name"]}',
-                f'{size}{r["playback"]["type"]}',
-                f'{size}{r["playback"]["format"]}',
-                f'{size}{str(r["playback"]["audio"])}',
-                f'{size}{str(r["playback"]["duration"])} s',
-                f'{size}{r["playback"]["datetime_start_str"]}',
-                f'{size}{r["playback"]["datetime_end_str"]}',
-            )
-            for r in active
-        ]
-        inactive = [
-            (
-                f'{size}{str(r["position"])}',
-                f'{size}{r["playback"]["aka"]}',
-                f'{size}{r["playback"]["file_name"]}',
-                f'{size}{r["playback"]["type"]}',
-                f'{size}{r["playback"]["format"]}',
-                f'{size}{str(r["playback"]["audio"])}',
-                f'{size}{str(r["playback"]["duration"])} s',
-                f'{size}{r["playback"]["datetime_start_str"]}',
-                f'{size}{r["playback"]["datetime_end_str"]}',
-            )
-            for r in inactive
-        ]
+    # create list of images for use with thumbnails
+    active_thumbnails = [i["thumbnail"] for i in active]
 
-    return active, inactive
+    # flatten information into plain list
+    active = flatten(active, 14)
+    inactive = flatten(inactive, 14)
+
+    return active, inactive, active_thumbnails
+
+
+def flatten(data, size):
+    # ("alert", color1, r["playback"]["file_name"]),
+    sz = f"[size={str(size)}]"
+    return [
+        (
+            f'{sz}{str(i["position"])}',
+            f'{sz}{i["playback"]["aka"]}',
+            f'{sz}{i["playback"]["file_name"]}',
+            f'{sz}{i["playback"]["type"]}',
+            f'{sz}{i["playback"]["format"]}',
+            f'{sz}{str(i["playback"]["audio"])}',
+            f'{sz}{str(i["playback"]["duration"])} s',
+            f'{sz}{i["playback"]["datetime_start_str"]}',
+            f'{sz}{i["playback"]["datetime_end_str"]}',
+        )
+        for i in data
+    ]
 
 
 class Table(BoxLayout):
@@ -83,11 +75,11 @@ class Table(BoxLayout):
         self.orientation = "vertical"
 
         # load active and inactive data into flat table format
-        active, inactive = load_storyboard(flat=True)
+        active, inactive, active_thumbnails = load_storyboard(flat=True)
         sh_top = len(active) / (len(active) + len(inactive))
         sh_bottom = 1 - sh_top
 
-        # split main box layout into two parts
+        # split main box layout into two parts (Widget A, Widget B)
         top_block = BoxLayout(
             orientation="vertical",
             spacing=15,
@@ -101,29 +93,71 @@ class Table(BoxLayout):
             size_hint=(1, sh_bottom),
         )
 
-        # populate upper box with title and table
-        new_widget = MDLabel(
+        # populate upper box with title, table and button section
+        new_table = MDLabel(
             markup=True,
             text="[b]Loaded[/b]",
             size_hint=(1, 0.1),
             halign="center",
         )
-        new_widget.font_size = 30
-        top_block.add_widget(new_widget)
-        top_block.add_widget(self.generate_table(active))
+        new_table.font_size = 30
+        new_midsection = BoxLayout(
+            orientation="horizontal",
+            spacing=5,
+            padding=(5, 5, 5, 5),
+            size_hint=(1, 0.8),
+        )
+        new_widget = BoxLayout(
+            orientation="horizontal",
+            spacing=5,
+            padding=(5, 5, 5, 5),
+            size_hint=(1, 0.1),
+        )
 
-        # populate lower box with title and table
-        new_widget = MDLabel(
+        # insert table and preview zone in midsection
+
+        image_preview = Image(
+            source=os.path.join(MEDIA_LOCATION, "thumbnails", active_thumbnails[0])
+        )
+        image_preview.size_hint = (0.25, 1)
+
+        new_midsection.add_widget(self.generate_table(active))
+        new_midsection.add_widget(image_preview)
+
+        # insert buttons into button section
+        new_widget.add_widget(Button(text="UnLoad"))
+        new_widget.add_widget(Button(text="ReOrder"))
+
+        # combine all three pieces of Widget A
+        top_block.add_widget(new_table)
+        top_block.add_widget(new_midsection)
+        top_block.add_widget(new_widget)
+
+        # populate upper box with title, table and button section
+        new_title = MDLabel(
             markup=True,
             text="[b]UnLoaded[/b]",
             size_hint=(1, 0.1),
             halign="center",
         )
-        new_widget.font_size = 30
-        bottom_block.add_widget(new_widget)
-        bottom_block.add_widget(self.generate_table(inactive))
+        new_title.font_size = 30
+        new_widget = BoxLayout(
+            orientation="horizontal",
+            spacing=5,
+            padding=(5, 5, 5, 5),
+            size_hint=(1, 0.1),
+        )
 
-        # add both blocks to main box layout
+        # insert buttons into button section
+        new_widget.add_widget(Button(text="Load"))
+        new_widget.add_widget(Button(text="Discard"))
+
+        # combine all three pieces of Widget B
+        bottom_block.add_widget(new_title)
+        bottom_block.add_widget(self.generate_table(inactive))
+        bottom_block.add_widget(new_widget)
+
+        # combine Widget A and Widget B
         self.add_widget(top_block)
         self.add_widget(bottom_block)
 
@@ -140,27 +174,31 @@ class Table(BoxLayout):
                 ("Start Date", dp(30)),
                 ("End Date", dp(30)),
             ],
-            background_color_header="#FF00FF",
-            background_color_selected_cell="e4514f",
-            check=True,
+            background_color_header="#b0bec5",
+            background_color_selected_cell="#e6b400",
+            elevation=2,
         )
         new_table.row_data = table_data
-        new_table.size_hint = (1, 0.9)
+        # new_table.size_hint = (1, 0.8)
+        new_table.size_hint = (0.75, 1)
         return new_table
 
 
 class KivyApp(MDApp):
     def build(self):
-        self.theme_cls.them_style = "Light"
-        self.theme_cls.primary_palette = "BlueGray"
+        #     self.theme_cls.them_style = "Light"
+        #     self.theme_cls.primary_palette = "BlueGray"
         return Table()
 
 
 def main():
+    global MEDIA_LOCATION
+    MEDIA_LOCATION = r"C:\pythonCode\rollerAds\media"
     kivy.require("2.1.0")
     Window.size = (1500, 900)
     Window.top = 50
     Window.left = 50
+    Window.clearcolor = (1, 0, 0, 1)
     KivyApp().run()
 
 
